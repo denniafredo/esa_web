2e8b8b3656e4f84c32d802db1416d7582e6a37d8
denniafredo <denniafredo@gmail.com>
2024-01-29 17:22:30 +0800

    add total and public holiday api

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,24 +3,65 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employment;
-use App\Models\EmploymentDivision;
-use App\Models\EmploymentRole;
-use App\Models\EmploymentStatus;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Carbon;
-
-// Adjust the namespace as needed
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $employments = Employment::whereDate('date_start_of_work', '>=', Carbon::now()->subWeek())->get();
-        return view('dashboard', compact('employments'))
+        // Fetch employments
+        $employments = Employment::whereDate('date_start_of_work', '>=', now()->subWeek())->get();
+        $totalEmployments = Employment::count();
+
+        // Fetch public holidays for the current month and year
+        $currentMonth = now()->month;
+        $currentYear = now()->year;
+
+        $url = "https://api-harilibur.vercel.app/api?year={$currentYear}";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            echo 'cURL error: ' . curl_error($ch);
+        }
+        curl_close($ch);
+
+        $publicHolidays = json_decode($response, true);
+        for ($i = 0; $i < sizeof($publicHolidays); $i++) {
+            $date = Carbon::createFromFormat('Y-m-d', $publicHolidays[$i]['holiday_date']);
+            $publicHolidays[$i]['day_of_week'] = $date->format('D');
+        }
+        $currentDate = now();
+        $filteredHolidays = [];
+        usort($publicHolidays, function ($a, $b) {
+            $dateA = Carbon::createFromFormat('Y-m-d', $a['holiday_date']);
+            $dateB = Carbon::createFromFormat('Y-m-d', $b['holiday_date']);
+            return $dateA <=> $dateB;
+        });
+        for ($i = 0; $i < sizeof($publicHolidays); $i++) {
+            $date = Carbon::createFromFormat('Y-m-d', $publicHolidays[$i]['holiday_date']);
+
+            if ($date->gte($currentDate)) {
+                $publicHolidays[$i]['day_of_week'] = $date->format('D');
+                $filteredHolidays[] = $publicHolidays[$i];
+            }
+
+            // Limit to 6 events
+            if (count($filteredHolidays) >= 6) {
+                break;
+            }
+        }
+        $publicHolidays = $filteredHolidays;
+
+        return view('dashboard', compact('employments', 'totalEmployments', 'publicHolidays'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
+
 
     public function show($id)
     {
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,7 +24,7 @@
                                                  data-type="percent"></div>
                                         </div>
                                         <div class="ml-3">
-                                            <h5 class="text-white font-weight-bold">57 <small> /100 Employee</small>
+                                            <h5 class="text-white font-weight-bold">0 <small> /100 Employee</small>
                                             </h5>
                                             <small class="mb-0">Time In</small>
                                         </div>
@@ -62,9 +62,28 @@
                                                  data-type="percent"></div>
                                         </div>
                                         <div class="ml-3">
-                                            <h5 class="text-white font-weight-bold">5 <small> /100 Employee</small>
+                                            <h5 class="text-white font-weight-bold">0<small> /100 Employee</small>
                                             </h5>
                                             <small class="mb-0">On Leave</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="card bg-success">
+                                <div class="card-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="fit-icon-2 text-info text-center">
+                                            <div id="circle-progress-03"
+                                                 class="circle-progress-03 circle-progress circle-progress-light"
+                                                 data-min-value="0" data-max-value="100" data-value="5"
+                                                 data-type="percent"></div>
+                                        </div>
+                                        <div class="ml-3">
+                                            <h5 class="text-white font-weight-bold">{{$totalEmployments}} <small>Employees</small>
+                                            </h5>
+                                            <small class="mb-0">TOTAL</small>
                                         </div>
                                     </div>
                                 </div>
@@ -76,104 +95,35 @@
                     <div class="card card-block card-stretch card-height">
                         <div class="card-header d-flex justify-content-between">
                             <div class="header-title">
-                                <h4 class="card-title">Upcoming Events</h4>
+                                <h4 class="card-title">Upcoming National Event</h4>
                             </div>
                         </div>
                         <div class="card-body p-0">
                             <div class="table-responsive">
                                 <table class="table table-spacing mb-0">
                                     <tbody>
-                                    <tr class="white-space-no-wrap">
-                                        <td>
-                                            <h6 class="mb-0 text-uppercase text-secondary">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="pr-2" width="30"
-                                                     fill="none"
-                                                     viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          stroke-width="2"
-                                                          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                                                </svg>
-                                                Tue, 30 June 2023
-                                            </h6>
-                                        </td>
-                                        <td class="pl-0 py-3">
-                                            Cuti Bersama
-                                        </td>
-                                    </tr>
-                                    <tr class="white-space-no-wrap">
-                                        <td>
-                                            <h6 class="mb-0 text-uppercase text-secondary">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="pr-2" width="30"
-                                                     fill="none"
-                                                     viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          stroke-width="2"
-                                                          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                                                </svg>
-                                                Monday, 09 July 2023
-                                            </h6>
-                                        </td>
-                                        <td class="pl-0 py-3">
-                                            Upacara Bendera
-                                        </td>
-                                    </tr>
-                                    <tr class="white-space-no-wrap">
-                                        <td>
-                                            <h6 class="mb-0 text-uppercase text-secondary">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="pr-2" width="30"
-                                                     fill="none"
-                                                     viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          stroke-width="2"
-                                                          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                                                </svg>
-                                                Sunday, 15 Aug 2023
-                                            </h6>
-                                        </td>
-                                        <td class="pl-0 py-3">
-                                            Townhall
-                                        </td>
-                                    </tr>
-                                    <tr class="white-space-no-wrap">
-                                        <td>
-                                            <h6 class="mb-0 text-uppercase text-secondary">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="pr-2" width="30"
-                                                     fill="none"
-                                                     viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          stroke-width="2"
-                                                          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                                                </svg>
-                                                Thursday, 26 Oct 2023
-                                            </h6>
-                                        </td>
-                                        <td class="pl-0 py-3">
-                                            Outing
-                                        </td>
-                                    </tr>
-                                    <tr class="white-space-no-wrap">
-                                        <td>
-                                            <h6 class="mb-0 text-uppercase text-secondary">
-                                                <svg xmlns="http://www.w3.org/2000/svg" class="pr-2" width="30"
-                                                     fill="none"
-                                                     viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          stroke-width="2"
-                                                          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
-                                                </svg>
-                                                Friday, 25 Dec 2023
-                                            </h6>
-                                        </td>
-                                        <td class="pl-0 py-3">
-                                            Sale
-                                        </td>
-                                    </tr>
+                                    @foreach($publicHolidays as $publicHoliday)
+                                        <tr class="white-space-no-wrap">
+                                            <td>
+                                                <h6 class="mb-0 text-uppercase {{ $publicHoliday['is_national_holiday'] ? 'text-danger' :'text-secondary'}}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" class="pr-2" width="30"
+                                                         fill="none"
+                                                         viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                              stroke-width="2"
+                                                              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                                                    </svg>
+                                                    {{$publicHoliday['day_of_week']}}
+                                                    , {{$publicHoliday['holiday_date']}}
+                                                </h6>
+                                            </td>
+                                            <td class=" pl-0 py-3">
+                                                {{$publicHoliday['holiday_name']}}
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                     </tbody>
                                 </table>
-
-                            </div>
-                            <div class="d-flex justify-content-start align-items-center border-top-table p-3">
-                                <button class="btn btn-secondary btn-sm">See All</button>
                             </div>
                         </div>
                     </div>
@@ -227,7 +177,7 @@
                                                 @endphp
                                                 <p class="mb-0 {{$style}} d-flex justify-content-start">
                                                     <small><i
-                                                            class="fas fa-circle mr-2"></i></small>{{$employment->employmentStatus->name}}
+                                                                class="fas fa-circle mr-2"></i></small>{{$employment->employmentStatus->name}}
                                                 </p>
                                             </td>
                                         </tr>
